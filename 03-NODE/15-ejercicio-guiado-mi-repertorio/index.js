91ef95a0bd4dfd4831f6d4e6731e63cb1c5fe8f2
abecerraguz <alejandro.becerra@publimetro.cl>
2022-06-23 17:26:05 -0400

    Se guardan los cambios

--- 03-NODE/15-ejercicio-guiado-mi-repertorio/index.js
+++ 03-NODE/15-ejercicio-guiado-mi-repertorio/index.js
@@ -20,14 +20,29 @@ http
         const nombre = params.nombre
         const contenido = params.contenido
 
-        if (req.url.includes('/')) {
-            res.write('Servidor funcionando!')
-            res.end()
-        }
+        // if (req.url.includes('/')) {
+        //     res.write('Servidor funcionando!')
+        //     res.end()
+        // }
 
         if (req.url.includes('/crear')) {
             fs.writeFile(nombre, contenido, () => {
-                res.write('Archivo creado con éxito!')
+                res.write(`
+                <html><head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <title>Archivo eliminado</title>
+    </head>
+        <body>
+            <div class="container">
+            <h1 class="h4 pt-3 pb-1">Archivo creado con éxito!</h1>
+            <hr>
+            </div>
+        </body>
+        </html>`)
                 res.end()
             })
         }
@@ -35,21 +50,64 @@ http
 
         if (req.url.includes('/leer')) {
             fs.readFile(nombre, (err, data) => {
-                res.write(data)
+                res.write(`<html><head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+                <title>Archivo eliminado</title>
+            </head>
+                <body>
+                <div class="container">
+                    ${data}
+                </div>
+                </body>
+                </html>`)
                 res.end()
             })
         }
 
         if (req.url.includes('/renombrar')) {
             fs.rename('Repertorio.txt', nombre, (err, data) => {
-                res.write(`Archivo Repertorio.txt renombrado por ${nombre}`)
+                res.write(`
+                <html><head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <title>Archivo eliminado</title>
+    </head>
+        <body>
+            <div class="container">
+            <h1 class="h4 pt-3 pb-1">Archivo Repertorio.txt renombrado por ${nombre}</h1>
+            <hr>
+            </div>
+        </body>
+        </html>`)
                 res.end()
             })
         }
 
         if (req.url.includes('/eliminar')) {
             fs.unlink(nombre, (err, data) => {
-                res.write(`Archivo ${nombre} eliminado con éxito`)
+                res.write(`
+                <html><head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <title>Archivo eliminado</title>
+    </head>
+        <body>
+            <div class="container">
+            <h1 class="h4 pt-3 pb-1">Archivo ${nombre} eliminado con éxito</h1>
+            <hr>
+            </div>
+        </body>
+        </html>`)
                 res.end()
             })
         }
